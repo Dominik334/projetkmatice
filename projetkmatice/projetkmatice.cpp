0bbe5d8e2a08bbd6bc07f72ea4eaba0385e07899
Dominik <Dominik@DESKTOP-TFIJ60M>
2023-11-26 19:04:50 +0100

    Přidejte soubory projektu.

---
+++ projetkmatice/projetkmatice.cpp
@@ -0,0 +1,38 @@
+﻿// projetkmatice.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "projetkmatice.h"
+using namespace std;
+
+int main()
+{
+	int riadky;
+	int stlpce;
+	int vstup;
+	printf("Zadaj pocet riadkov a stlpcov");
+	scanf("%d %d", &riadky, &stlpce);
+	int matica[5][5];
+
+		printf("Zadaj prvok matice: ");
+		for (int k = 0; k < riadky; k++)
+		{
+			printf("\n");
+			for (int j = 0; j < stlpce; j++)
+			{
+				scanf("%d", &vstup);
+				matica[k][j] = vstup;
+			}
+		}
+
+	printf("\nMatica je : \n");
+	for (int k = 0; k < riadky; k++)
+	{
+		printf("\n");
+		for (int j = 0; j < stlpce; j++)
+		{
+			printf("%d\t", matica[k][j]);
+		}
+	}
+printf("koniec");
+	return 0;
+}
